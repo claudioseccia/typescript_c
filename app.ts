c7c2108c09bd5625b0a9ec80d9913ef368d1f7c9
claudioseccia <cla.seccia@gmail.com>
2022-10-21 11:45:27 +0200

    First commit

---
+++ app.ts
@@ -0,0 +1,5 @@
+//tsc app.ts <-- to compile in js
+//npm init
+//npm install --save-dev lite-server
+//npm start <-- lite server opens a browser page, each time we change something we need to run: tsc app.ts
+console.log('Your code goes here...');
