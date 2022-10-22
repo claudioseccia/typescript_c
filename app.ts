91e1916ba5f435bcb307e83128170b530614241d
claudioseccia <cla.seccia@gmail.com>
2022-10-22 18:59:24 +0200

    Type aliases custom types

--- app.ts
+++ app.ts
@@ -20,6 +20,7 @@ console.log(combinedNames);
 //****************************************
 //2.14 LITERAL TYPES
 //types that specify the exact value and type
+/*
 function combine(
   input1: number | string,
   input2: number | string,
@@ -39,6 +40,34 @@ function combine(
   //   } else {
   //     return result.toString(); //concatenats as a string
   //   }
+  return result;
+}
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
+*/
+//****************************************
+//2.15 TYPE ALIASES CUSTOM TYPES
+//type keyword
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
   return result;
 }
 const combinedAges = combine(30, 26, "as-number");
