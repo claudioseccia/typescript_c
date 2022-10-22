7dd5be9b87607dabbf867c4a1e7a7bfb4dfd5377
claudioseccia <cla.seccia@gmail.com>
2022-10-22 18:52:26 +0200

    2.14 Literal Types

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@
 // npm run
 //****************************************
 //2.13 UNION TYPES
+/* 
 function combine(input1: number | string, input2: number | string) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
@@ -15,5 +16,34 @@ const combinedAges = combine(30, 26);
 console.log(combinedAges);
 const combinedNames = combine("Max", "Anna");
 console.log(combinedNames);
+*/
 //****************************************
 //2.14 LITERAL TYPES
+//types that specify the exact value and type
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  //   if (resultConversion === "as-number") {
+  //     return +result; //return result as a number
+  //   } else {
+  //     return result.toString(); //concatenats as a string
+  //   }
+  return result;
+}
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
