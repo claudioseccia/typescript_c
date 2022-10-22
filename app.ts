e35148c4375c066aa8e47894a09023913ca0015a
claudioseccia <cla.seccia@gmail.com>
2022-10-22 19:06:00 +0200

    2.16 Type Aliases and object types

--- app.ts
+++ app.ts
@@ -76,3 +76,27 @@ const combinedStringAges = combine("30", "26", "as-number");
 console.log(combinedStringAges);
 const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
+//****************************************
+//2.16 TYPE ALIASES AND OBJECT TYPES
+// Type aliases can be used to "create" your own types.
+// You're not limited to storing union types though - you can also provide an alias to a (possibly complex) object type.
+// For example:
+// type User = { name: string; age: number };
+// const u1: User = { name: 'Max', age: 30 }; // this works!
+
+// This allows you to avoid unnecessary repetition and manage types centrally.
+// For example, you can simplify this code:
+// function greet(user: { name: string; age: number }) {
+//   console.log('Hi, I am ' + user.name);
+// }
+// function isOlder(user: { name: string; age: number }, checkAge: number) {
+//   return checkAge > user.age;
+// }
+// To:
+type User = { name: string; age: number };
+function greet(user: User) {
+  console.log("Hi, I am " + user.name);
+}
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
