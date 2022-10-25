89e54c19369e1b18fb2dcf0dca93ed376af91006
claudioseccia <cla.seccia@gmail.com>
2022-10-25 16:11:50 +0200

    4.2 Let and Const

---
+++ CH_3/src/app.ts
@@ -0,0 +1,123 @@
+//****************************************
+//3.2 Live/Watch Mode in compile time
+//LIVE MODE COMPILE!!!
+// tsc app.js --watch
+//or
+// tsc app.js -w
+//--> watch mode: re-compile at any change
+// const userName = "Maximilian";
+// // userName = 3; //!!! ERROR !!!
+// console.log(userName);
+
+//****************************************
+//3.3 Compiling the entire project multiple files
+// tsc --init       <-- it creates a file: tsconfig.json with many rules to configure
+//with the command:
+// tsc
+//we now compile all typescript files in the project
+// tsc --watch      <-- to go in watch mode (ctrl+c to exit)
+//****************************************
+//3.4 Including and excluding files
+// see tsconfig.jscon in the "exclude" section
+
+//****************************************
+//3.4 Setting a compilation target
+//see "compilerOptions": { "target" .... option in tsconfig.json
+
+//****************************************
+//3.5 Understanding typescript core libs
+// const button = document.querySelector("button")!;
+// button.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+//if lib[] entry is not set in tsconfig.jscon all defaults like DOM are included (like running the js in the browser)
+//uncommenting we HAVE TO set in the array some values
+//ex these are the default settings (like if "lib":[] entry is commented)
+// "lib": [
+//     "dom",
+//     "es6",
+//     "dom.iterable",
+//     "scripthost"
+// ]
+
+//****************************************
+//3.6 More configuration and compilation options
+//in tsconfig.json
+//"allowjs": true   //<-- includes js files
+//"checkjs": true   //<-- checks js files
+
+//****************************************
+//3.7 Working with Source Maps
+//source maps helps us on development and debugging
+//generates a .js.map file too and it makes visible .ts typescript files in the chrome dev tools
+// in this way we can even set breakpoints to the ts code to debug !!!
+
+//****************************************
+//3.8 rootDir and outDir
+//typically we have a src folder and a dist folder in our projects
+//src folder will hold all our typescript files
+//dist folder all the compiled files (all the javascript files)
+
+// to output all the compiled files into dist we set "outDir" as dist
+// if we create a subfolder and place file inside it the folder structure is replicated into dist folder
+
+// to make ts compiler watch only at src and its structure we set it as: "rootDir": "./src"
+//in this way files on other folders will be ignored, otherwise all files and folder also in the root will be watched
+
+//"removeComments": true, <-- this removes comments from compilation
+//"noEmit": true, <-- this will not export js
+
+//****************************************
+//3.9 Stop emitting files on compilation errors
+// "noEmitOnError": true, <-- default is false (when commented)
+// setting it on true js will not be generated when an error is detected by js
+
+//****************************************
+//3.10 Strict compilation
+//setting: "strict": true, <-- is the same than setting all the separate settings below separately
+
+//noImplicitAny --> see analytics.ts
+
+//strictNullChecks --> if set to false it's not necessary the exclamation mark here to check if the button exists:
+// const button = document.querySelector("button"); //typescript doesn't know if button does not exist at compilation time
+// ! -->exclamation mark tells typescript about elements the dev knowns do exists
+//otherwise, without using ! do:
+// if(button) {
+//   button.addEventListener("click", () => {
+//     console.log("clicked");
+//   });
+// }
+
+//strictBindCallApply
+const button = document.querySelector("button")!;
+
+function clickHandler(msg: string) {
+  console.log("clicked " + msg);
+}
+button.addEventListener("click", clickHandler.bind(null, "You're wellcome!"));
+
+//****************************************
+//3.11 Code quality options
+//noUnusedLocals
+//throw errors if variables are set but not used etc... (unused global variables are allowed)
+
+//noUnusedParameters
+//checks for unused parameters in functions
+
+//noImplicitReturns etc...
+//this function does not always return something (ex. cases outside if statement)
+// function add(n1: number, n2: number) {
+//   if (n1 + n2 > 0) {
+//     return n1 + n2;
+//   }
+// }
+
+//****************************************
+//3.12 Debug in Vscode
+//place a breakpoint
+//set in launch.json port of the debugging page
+//set "sourceMap": true in tsconfig.json
+//tsc --watch
+//run debug
+//select chrome browser
+//-->it will open a window in chrome to run debugging
--- src/app.ts
+++ src/app.ts
@@ -1,123 +1,56 @@
-//****************************************
-//3.2 Live/Watch Mode in compile time
-//LIVE MODE COMPILE!!!
-// tsc app.js --watch
-//or
-// tsc app.js -w
-//--> watch mode: re-compile at any change
-// const userName = "Maximilian";
-// // userName = 3; //!!! ERROR !!!
-// console.log(userName);
+//ES6+
 
 //****************************************
-//3.3 Compiling the entire project multiple files
-// tsc --init       <-- it creates a file: tsconfig.json with many rules to configure
-//with the command:
-// tsc
-//we now compile all typescript files in the project
-// tsc --watch      <-- to go in watch mode (ctrl+c to exit)
-//****************************************
-//3.4 Including and excluding files
-// see tsconfig.jscon in the "exclude" section
+//4.2 Let and Const
+//CONST
+const userName = "Max";
+// userName = "Bill"; //!!! ERROR !!! Cannot re-declare constant
 
-//****************************************
-//3.4 Setting a compilation target
-//see "compilerOptions": { "target" .... option in tsconfig.json
+//LET
+let age = 26;
+age = 29; //can be changed
 
-//****************************************
-//3.5 Understanding typescript core libs
-// const button = document.querySelector("button")!;
-// button.addEventListener("click", () => {
-//   console.log("clicked");
-// });
-//if lib[] entry is not set in tsconfig.jscon all defaults like DOM are included (like running the js in the browser)
-//uncommenting we HAVE TO set in the array some values
-//ex these are the default settings (like if "lib":[] entry is commented)
-// "lib": [
-//     "dom",
-//     "es6",
-//     "dom.iterable",
-//     "scripthost"
-// ]
+//VAR vs LET
+//VAR
+//var should not be used anymore
+//difference between var and let
 
-//****************************************
-//3.6 More configuration and compilation options
-//in tsconfig.json
-//"allowjs": true   //<-- includes js files
-//"checkjs": true   //<-- checks js files
-
-//****************************************
-//3.7 Working with Source Maps
-//source maps helps us on development and debugging
-//generates a .js.map file too and it makes visible .ts typescript files in the chrome dev tools
-// in this way we can even set breakpoints to the ts code to debug !!!
-
-//****************************************
-//3.8 rootDir and outDir
-//typically we have a src folder and a dist folder in our projects
-//src folder will hold all our typescript files
-//dist folder all the compiled files (all the javascript files)
-
-// to output all the compiled files into dist we set "outDir" as dist
-// if we create a subfolder and place file inside it the folder structure is replicated into dist folder
-
-// to make ts compiler watch only at src and its structure we set it as: "rootDir": "./src"
-//in this way files on other folders will be ignored, otherwise all files and folder also in the root will be watched
-
-//"removeComments": true, <-- this removes comments from compilation
-//"noEmit": true, <-- this will not export js
-
-//****************************************
-//3.9 Stop emitting files on compilation errors
-// "noEmitOnError": true, <-- default is false (when commented)
-// setting it on true js will not be generated when an error is detected by js
-
-//****************************************
-//3.10 Strict compilation
-//setting: "strict": true, <-- is the same than setting all the separate settings below separately
-
-//noImplicitAny --> see analytics.ts
-
-//strictNullChecks --> if set to false it's not necessary the exclamation mark here to check if the button exists:
-// const button = document.querySelector("button"); //typescript doesn't know if button does not exist at compilation time
-// ! -->exclamation mark tells typescript about elements the dev knowns do exists
-//otherwise, without using ! do:
-// if(button) {
-//   button.addEventListener("click", () => {
-//     console.log("clicked");
-//   });
+//var has a global and function scope
+// function add(a: number, b: number) {
+//   var result;
+//   result = a + b;
+//   return result;
 // }
+// console.log(result); //!!! ERROR !!! var is defined only inside the scope of add function
 
-//strictBindCallApply
-const button = document.querySelector("button")!;
-
-function clickHandler(msg: string) {
-  console.log("clicked " + msg);
-}
-button.addEventListener("click", clickHandler.bind(null, "You're wellcome!"));
-
-//****************************************
-//3.11 Code quality options
-//noUnusedLocals
-//throw errors if variables are set but not used etc... (unused global variables are allowed)
-
-//noUnusedParameters
-//checks for unused parameters in functions
-
-//noImplicitReturns etc...
-//this function does not always return something (ex. cases outside if statement)
-// function add(n1: number, n2: number) {
-//   if (n1 + n2 > 0) {
-//     return n1 + n2;
-//   }
+// var result;
+// function add(a: number, b: number) {
+//   result = a + b;
+//   return result;
 // }
+// console.log(result); //WORKS! result is defined at global scope
 
-//****************************************
-//3.12 Debug in Vscode
-//place a breakpoint
-//set in launch.json port of the debugging page
-//set "sourceMap": true in tsconfig.json
-//tsc --watch
-//run debug
-//select chrome browser
-//-->it will open a window in chrome to run debugging
+// age = 29;
+// if (age > 20) {
+//   var isOld = true; //var registers a variable always globally
+// }
+// console.log(isOld); //error in typescript but fine in javascript
+
+//LET
+//LET AND CONST add the concept of block scope
+//any let or const are valid only inside a block or a nested block inside the one where they are defined
+// {
+//     let variable = 'cool';
+//     {
+//         console.log(variable); //WORKS!
+//     }
+//     console.log(variable); //WORKS!
+// }
+// console.log(variable); //!!! ERROR !!!
+
+//ex.
+// age = 29;
+// if (age > 20) {
+//   let isOld = true; //var registers a variable always globally
+// }
+// console.log(isOld); //!!! ERROR !!! isOld is not defined
