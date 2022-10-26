b93ac33bcafbadb6ba1b9a3254cd6133d6b7f8c4
claudioseccia <cla.seccia@gmail.com>
2022-10-26 10:48:22 +0200

    5.4 Compiling to javascript

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@
 //****************************************
 //5.3 Creating a fist class
 //convention is to start with uppercase char for class names
+//ES6 syntax:
 class Department {
   name: string;
   //constructor() is executed when the object is created
@@ -14,3 +15,7 @@ class Department {
 //create a first object based on the class "blueprint":
 const accounting = new Department("Accounting");
 console.log(accounting);
+
+//****************************************
+//5.4 Compiling to javascript
+//changing to ES5 into tsconfig ("target": "ES5"), it will change in the compilation with the old ES5 syntax
