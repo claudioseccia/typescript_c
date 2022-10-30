1d3300aa65fd836d6a22b5de7d9da1de986d4ecd
claudioseccia <cla.seccia@gmail.com>
2022-10-30 18:45:59 +0100

    5.17 - Using Interfaces with Classes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@
 //DEFINITION: an interface describes an object
 //only typescript!!!
 //values can NOT be put into an interface (ex: name:string = 'Peter')
+/*
 interface Person {
   name: string;
   age: number;
@@ -21,3 +22,47 @@ user1 = {
   },
 };
 user1.greet("Hi there! I am");
+*/
+//****************************************
+//5.17 - Using Interfaces with Classes
+//difference between type and interface
+// type Person = {
+//     name: string;
+//     age: number;
+//     greet(phrase: string): void;
+// }
+//it will work the same
+
+//differences:
+//1) interfaces can only be used to describe the structure of an object
+//2) inside a custom type we can set union types inside it (looks like they are more flexible, however...)
+//3) an interface looks more clear to define an object, AND you can implement an interface in a class
+//   an interface can be used like a "contract" a class can implement, like the minimum properties to have inside it
+//4) an interface can share functionalities among different classes
+//
+//difference between inheritance and interface:
+//you can inherit only from one class, but an interface can be a blueprint for more than one class
+//
+//difference between abstract class and interface:
+//interface has not implementation details at all, an abstract class has (and can be overridden)
+interface Greetable {
+  name: string;
+  greet(phrase: string): void;
+}
+//here name property and greet method have to be implemented, since the class Person implements Greetable interface:
+class Person implements Greetable {
+  //can inherit from more than one interface like: class Person implements Greetable, AnotherInterface { ....
+  name: string;
+  age = 30; //more properties, over the original interface
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string) {
+    //implementation of the greet method described in the interface
+    console.log(phrase + " " + this.name);
+  }
+}
+let user1: Greetable; //let user1: Person; <- works too
+user1 = new Person("Max");
+user1.greet("Hi there, my name is");
+console.log(user1);
