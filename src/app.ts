bfa5905dd35c71867623db777d9ee1689f7d9450
claudioseccia <cla.seccia@gmail.com>
2022-11-04 00:56:13 +0100

    7.3 - Creating a Generic Function

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@
 // const names: Array = ["Max", "Manu"]; //ERROR !!! require at least one type
 // const names: any[] = ["Max","Manu",2]; //Ugly but works
 // const names: Array<string> = ["Max", "Manu"]; //WORKS! -- same as const names: string[] = ...
+/*
 const names: Array<string | number> = ["Max", "Manu", 2]; //WORKS (union types) - Array<any> <-- to set any type
 
 //PROMISE TYPE
@@ -36,3 +37,40 @@ promise.then((data) => {
 //and
 //resolve(10);
 //typescript will give error on: data.split(" "); --> split is a type only for strings
+*/
+//****************************************
+//7.3 - Creating a Generic Function
+/*
+//ex. function that takes as a param two objects and returns an object
+function merge(objA: object, objB: object) {
+  return Object.assign(objA, objB);
+}
+console.log(merge({ name: "Max" }, { age: 20 }));
+const mergedObject = merge({ name: "Max" }, { age: 20 });
+// console.log(mergedObject.name); //ERROR!!!   typescript doesn't know the type of the returned values
+// console.log(mergedObject.age); //ERROR!!!   typescript doesn't know the type of the returned values
+// we can do type casting as like: 
+//const mergedObject = merge({ name: "Max" }, { age: 20 }): as {name: string, age:number};
+//but it's cumbersome. Here's another solution: GENERICS
+*/
+//SOLUTION: <GENERICS>
+//since "object" as a type is a highly un-specified type
+//in this Generic assignment T and U are treated as any possible value extending object. Typescript automatically infers
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  //optional (TS automatically infers): ...(objA: T, objB: U): T & U
+  return Object.assign(objA, objB);
+}
+const mergedObject = merge(
+  { name: "Max", hobbies: ["sport", "develpment"] },
+  { age: 30 }
+);
+//not necessary! TS automatically infers values from generic types:
+// const mergedObject = merge<{name: string, hobbies: string[]},{age: number}>({ name: "Max", hobbies: ["sport", "develpment"] },{ age: 30 });
+console.log("mergedObject age:", mergedObject.age); //WORKS!
+console.log("mergedObject age:", mergedObject.hobbies); //WORKS!
+//THIS GIVES A LOT OF FLEXIBILITY, CREATING GENERIC TYPES TO ACCESS ANY PROPERTY INSIDE THE OBJECT
+
+const mergedObject2 = merge({ name: "Frank" }, { age: 20 });
+console.log("mergedObject2 name:", mergedObject2.name); //WORKS!
+//****************************************
+//7.
