67130994a2b639942c0e7654a6b9e0ca524150a0
claudioseccia <cla.seccia@gmail.com>
2022-11-11 01:23:09 +0100

    9.14 - Using a Getter

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,8 @@
 //****************************************
 //9.13 - Rendering Project Items with a Class
 //
+//****************************************
+//9.14 - Using a Getter
 //
 //****************************************************************************************************
 //Project Type
@@ -252,6 +254,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 //
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+  //getter to get the proper number of people
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id); //"single-project" ->template id, hostId -> host element ID, insert at the beginning (true), id -> id to be assigned to the newly created element
     this.project = project;
@@ -262,8 +272,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   renderContent() {
     //added h2, h3 and p to the id "single-project" in index.html
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned"; //this.persons triggers the getter (get persons())
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
