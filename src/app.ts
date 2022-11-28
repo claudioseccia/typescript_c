6d8568a6e80d77b6d71dc0172917eb203ca3884d
claudioseccia <cla.seccia@gmail.com>
2022-11-28 13:06:51 +0100

    12.04 - No Types Needed class-transformer

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,44 @@
 //****************************************
 //12.03 - Using declare as a Last Resort
 //add a script inside the body of index.html, and then use "declare var" syntax to get the set globally variable value
-
+/*
 import _ from "lodash";
 declare var GLOBAL: any; //declare syntax, executed after the boundle in the index.html (defer keyword there)
 console.log(_.shuffle([1, 2, 3, 4]));
 console.log(GLOBAL);
+*/
+//****************************************
+//12.04 - No Types Needed class-transformer
+//normal scenario
+/* 
+import { Product } from "./product.model";
+const products = [
+  { title: "A carpet", price: 29.99 },
+  { title: "A book", price: 10.99 },
+];
+// const p1 = new Product("A book", 99);
+// console.log(p1.getInformation()); //['A book', '$99']
+const loadedProducts = products.map((prod) => {
+  return new Product(prod.title, prod.price);
+});
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation()); //prints the info on products
+} 
+*/
+//CLASS-TRANSFORMER
+//https://www.npmjs.com/package/class-transformer
+//npm install class-transformer --save
+//npm install reflect-metadata --save
+
+import "reflect-metadata";
+import { plainToInstance } from "class-transformer";
+import { Product } from "./product.model";
+const products = [
+  { title: "A carpet", price: 29.99 },
+  { title: "A book", price: 10.99 },
+];
+// plainToClass(class we want to convert to, data to transform)
+const loadedProducts = plainToInstance(Product, products);
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation()); //prints the info on products, same as 12.04 code
+}
