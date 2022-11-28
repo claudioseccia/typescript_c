3bc6a3609f2257c3d5bdfce90d752b690dd3877c
claudioseccia <cla.seccia@gmail.com>
2022-11-28 12:45:02 +0100

    12.03 - Using declare as a Last Resort

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,11 @@
 //seach for @types/ANYPOPULARJAVASCRIPTLIBRARY, ex: @types/jquery
 //
 //****************************************
+//****************************************
+//12.03 - Using declare as a Last Resort
+//add a script inside the body of index.html, and then use "declare var" syntax to get the set globally variable value
 
 import _ from "lodash";
+declare var GLOBAL: any; //declare syntax, executed after the boundle in the index.html (defer keyword there)
 console.log(_.shuffle([1, 2, 3, 4]));
+console.log(GLOBAL);
